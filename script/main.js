fbdeb801efe30ca8500b140baac843e43d57ed15
shubh200112 <120493921+shubh200112@users.noreply.github.com>
2025-01-14 00:46:20 +0530

    Update main.js

--- script/main.js
+++ script/main.js
@@ -55,7 +55,7 @@ const animationTimeline = () => {
   const tl = new TimelineMax();
 
   tl
-    .to(".container", 0.4, {
+    .to(".container", 0.8, {
       visibility: "visible"
     })
     .from(".one", 0.7, {
